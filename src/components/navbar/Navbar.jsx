68c9dc1a5e5eeb2969bdd36b83c7b723c0ee8ba3
munishkumr5028 <munishkumr786@gmail.com>
2025-04-28 15:02:52 +0530

    Change website title name

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -59,7 +59,7 @@ function Navbar() {
       </div>
 
       <div className="main-navbar">
-        <div className="logo">Panjab University</div>
+        <div className="logo">PU Chandigarh</div>
         <nav className={`nav-links ${showMenu ? "active" : ""}`}>
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
